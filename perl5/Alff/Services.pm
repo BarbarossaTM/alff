dccc7b38cbd11178252eef1aec21db843bcf6312
Maximilian Wilhelm <mwilhelm@galois.math.uni-paderborn.de>
2006-05-31 17:38:42 +0200

    Support for new style service handling

--- perl5/Alff/Services.pm
+++ perl5/Alff/Services.pm
@@ -16,21 +16,47 @@ use strict;
 use Alff::Config;
 use Alff::Main;
 
-my $debug = 0;
-my $dry_run = 0;
+##
+# Little bit of magic to simplify debugging
+sub _options(@) { #{{{
+        my %ret = @_;
+
+        if ( $ret{debug} ) {
+                foreach my $opt (keys %ret) {
+                        print STDERR "Alff::Services->_options: $opt => $ret{$opt}\n";
+                }
+        }
+
+        return \%ret;
+} #}}}
 
 ##
 # Create new instance
+# 
+# Available options:
+#  * debug -> Print degub information? (default: 0)
+#  * services_d -> Path to service configuration dir (default: /etc/alff/services.d)
+#
 sub new { #{{{
 	my $self = shift;
 	my $class = ref($self) || $self;
 
+	# Get options
+	my $args = &_options;
+
+	# Default options, if unset
+	my $debug = $args->{debug} || 0;
+	my $services_d = $args->{services_d} || "/etc/alff/services.d";
+
+	# Create instances for used objects
 	my $config = Alff::Config->new( debug => $debug );
-	my $alff = Alff::Main->new( debug => $debug, dry_run => $dry_run );
+	my $alff = Alff::Main->new( debug => $debug );
 
 	my $obj = bless {
+		alff_main => $alff,
 		config => $config,
-		alff_main => $alff
+		debug => $debug,
+		services_d => $services_d,
 		}, $class;
 
 	return $obj;
@@ -84,6 +110,137 @@ sub allowWorldOpenServices() { #{{{
 	print " * Creating rule to allow access to services configured to be world-wide available...";
 	print $main->write_cmd("iptables -A FORWARD -j allowWorldOpenServices") ? "done.\n" : "FAILED\n";
 } #}}}
+
+################################################################################
+#			    Service chains handling			       #
+################################################################################
+
+##
+# Read
+sub readServiceNames() { #{{{
+	my $self = shift;
+	my $service_d = $self->{serivce_d};
+	my @services;
+
+	return undef if ( ! -d $service_d );
+
+	if ( ! opendir( SERVICE_D, $service_d ) ) {
+		print STDERR "Error: Cannot read content of $service_d\n";
+		return undef;
+	}
+
+	@services = grep { ! /^\.{1,2}$/ } readdir(SERVICE_D);		# weed out "." and ".."
+	closedir( SERVICE_D );
+
+	return @services;
+} #}}}
+
+##
+# Read the configuration file for this service and return it.
+sub readServiceConfiguration($) { #{{{
+	my $self = shift;
+	my $service = shift;
+
+	my $services_d = $self->{services_d};
+	my $service_file = "$services_d/$service";
+
+	return undef if ( ! -d $services_d );
+	return undef if ( ! -f $service_file );
+
+	# Read $service_config from config file
+	my $service_config = undef;
+	require "$service_file";
+
+	return $service_config;
+} #}}}
+
+##
+# Generate the service chain for service $_
+sub generateServiceChain($) { #{{{
+	my $self = shift;
+	my $service = shift;
+
+	my $alff = $self->{alff_main};
+
+	my $serviceconfig = $self->readServiceConfiguration( $service );
+	my $srv_chain = "allowSrv${service}";
+
+	# Check for config hash
+	unless ( $serviceconfig ) {
+		print STDERR "Error: Could not read configuration for service $service, skipping...\n";
+		return 0;
+	}
+
+	# Check it 'servers' is specifed
+	unless ( defined $serviceconfig->{servers} ) {
+		print STDERR "Error: You need to define at least one server for service $service, skipping...\n";
+		return 0;
+	}
+
+	# Check if 'ports' is specifed
+	unless ( defined $serviceconfig->{ports} ) {
+		print STDERR "Error: You need to define at least one server for service $service, skipping...\n";
+		return 0;
+	}
+
+	# Create a chain for this service
+	$alff->create_chain( $srv_chain, "filter" );
+
+	my @servers = split( /\ /, $serviceconfig->{servers} );
+	my @ports = split(/\ /, $serviceconfig->{ports} );
+
+	my @valid_ports = $self->validate_ports( $service, @ports );
+
+	foreach my $server ( @servers ) {
+		foreach my $service_port ( @valid_ports ) {
+			if ( m/(\d+)\/(tcp|udp)/ ) {
+				my ( $port, $proto ) = ( $1, $2 );
+				$alff->write_cmd( "iptables -A $srv_chain -p $proto -d $server --dport $port -j ACCEPT" );
+			}
+		}
+	}
+
+	# Allow service for configured security classes
+	foreach my $config_key ( keys %{$serviceconfig} ) {
+		# search for 
+		if ( m/allow_from_(\w+)_networks/ ) {
+			my $srv_class = $1;
+			my $srv_class_chain = "allowServicesFrom${srv_class}Nets";
+
+			if ( $alff->chain_exists( $srv_class_chain ) ) {
+				$alff->write_cmd( "iptables -A $srv_class_chain -j $srv_chain" );
+			} else {
+				print STDERR "Error: Service $service should be accessable from undefined security class $srv_class, skipping...\n";
+			}
+		}
+	}
+
+	# public accessable service?
+	if ( defined $serviceconfig->{allow_from_world} and $serviceconfig->{allow_from_world} eq "yes" ) {
+		$alff->write_cmd( "iptables -A allowWorldOpenServices -j $srv_chain" );
+	}
+} #}}}
+
+##
+# validate_ports( @ports )
+sub validate_ports($@) { #{{{
+	my $self = shift;
+	my $service = shift;	# the service name (used for error message)
+	my @ports = @_;		# the ports
+
+	my @valid_ports;
+
+	foreach my $port_spec ( @ports ) {
+		if ( m/(\d+)\/(tcp|udp)/ and $1 ge 0 and $1 le 65535 ) {
+			push @valid_ports, $port_spec;
+		} else {
+			print STDERR "Error in service configuration for $service, invalid port $port_spec, removing from list...\n";
+		}
+	}
+
+	return @valid_ports;
+} #}}}
+
 
 1;
 # vim:foldmethod=marker
