20ebe9572f1a3c55f3fd6dabcbcbd0e25e8e60d9
Maximilian Wilhelm <max@rfc2324.org>
2006-11-26 02:21:58 +0100

    Removed old 'run_cmd' method from Alff::Main which is not used anymore.
    Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

--- perl5/Alff/Main.pm
+++ perl5/Alff/Main.pm
@@ -49,44 +49,6 @@ sub new() { #{{{
 		}, $class;
 } #}}}
 
-##
-# system-wrapper with error checking
-sub run_cmd(@) { #{{{
-	my $self = shift;
-	my $cmd = shift;
-	my @args = shift || ();
-
-	if ( $self->{debug} ) {
-		if ( @args ) {
-			print STDERR "run \"$cmd\" with args " . join(" ", @args ) . "\n";
-		} else {
-			print STDERR "run \"$cmd\"\n";
-			
-		}
-	}
-	
-	return 0 if ( $self->{dry_run} );
-
-	my $ret = system( $cmd, @args );
-	
-	if ( $ret == -1 ) {
-		printf STDERR "Error: Failed to execute %s with args %s\"",
-			$cmd, join(" ", @args);
-	}
-	elsif ( $ret & 127 ) {
-		printf STDERR "Error while executing %s, child died with signal %d, %s coredump\n",
-			$cmd, ($? & 127), ($? & 128) ? 'with' :  'without';
-	}
-	elsif ( $ret ) {
-		printf STDERR "Error while executing %s with args %s, child exited with value %d\n",
-			$cmd, join(" ", @args), $? >> 8;
-	}
-
-	# Be aware of the different true/false idea between shell and perl
-	# Exit code 0 -> 1 in perl and vice versa.
-	return $ret == 0 ? 1 : 0 ;
-} #}}}
-
 
 ################################################################################
 #				write out stuff				       #
