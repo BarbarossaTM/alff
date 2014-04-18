dc357b50a88f7ba9327bbe43ac82ab0510e0d14f
Maximilian Wilhelm <max@rfc2324.org>
2014-04-18 23:24:50 +0200

    Add Copyright notice and GPLv2 license header for every new file.
    Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

--- python/alff/ruleset.py
+++ python/alff/ruleset.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 #
+#  A Linux Firewall Framework
+#
+#  Copyright (C) 2014 Maximilian Wilhelm <max@rfc2324.org>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 # Maximilian Wilhelm <max@rfc2324.org>
 #  --  Sat 12 Apr 2014 10:40:43 PM CEST
 #
